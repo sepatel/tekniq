96f7fd6b059c471b3f6b348c11f3ecd68c4cf600
Sejal Patel <sejal@tekniq.io>
2017-05-06 16:30:29 -0400

    Fixed issue with classpath properties not found
    If a classpath properties file was not found, an exception was always
    thrown instead of only being thrown if the stopOnFailure was set to
    true. Also tweaked it so that the error message specifies the name of
    the missing resource when it is a classpath based resource missing.

--- tekniq-core/src/main/kotlin/io/tekniq/config/TqPropertiesConfig.kt
+++ tekniq-core/src/main/kotlin/io/tekniq/config/TqPropertiesConfig.kt
@@ -1,6 +1,7 @@
 package io.tekniq.config
 
 import java.io.File
+import java.io.FileNotFoundException
 import java.util.*
 
 open class TqPropertiesConfig(private val propertiesFile: String, private val stopOnFailure: Boolean = true) : TqMapConfig(loadProperties(propertiesFile, stopOnFailure)) {
@@ -12,7 +13,11 @@ private fun loadProperties(propertiesFile: String, stopOnFailure: Boolean) = Pro
     if (propertiesFile.startsWith("classpath:")) {
         val filename = propertiesFile.substring("classpath:".length)
         val stream = javaClass.getResourceAsStream(filename) ?: TqPropertiesConfig::class.java.classLoader.getResourceAsStream(filename)
-        load(stream)
+        if (stream != null) {
+            load(stream)
+        } else if (stopOnFailure) {
+            throw FileNotFoundException("Resource $propertiesFile could not be read")
+        }
     } else {
         try {
             load(File(propertiesFile).inputStream())
