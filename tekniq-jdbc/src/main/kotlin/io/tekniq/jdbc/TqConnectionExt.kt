8900670aa4e64c5a782e359509322c1f95ec4550
Sejal Patel <sejal@tekniq.io>
2017-05-13 20:40:49 -0400

    Updated to Java8 Target binaries
    Cleaned up Database Extensions to utilize "use" feature of Java8. The
    code was always intended for Java 8 but only the source compatibility
    was set before. Now the target compatiblity flag has also been enabled
    and the jre8 version of the kotlin standard libraries are incorporated.

--- tekniq-jdbc/src/main/kotlin/io/tekniq/jdbc/TqConnectionExt.kt
+++ tekniq-jdbc/src/main/kotlin/io/tekniq/jdbc/TqConnectionExt.kt
@@ -5,74 +5,71 @@ import java.time.*
 import java.util.*
 import java.util.Date
 
-fun Connection.select(sql: String, vararg params: Any?, action: ResultSet.() -> Unit) {
-    val stmt = prepareStatement(sql).apply { applyParams(this, *params) }
-    val rs = stmt.executeQuery()
-    while (rs.next()) {
-        action.invoke(rs)
-    }
-    rs.close()
-    stmt.close()
-}
+fun Connection.select(sql: String, vararg params: Any?, action: ResultSet.() -> Unit)
+        = prepareStatement(sql)
+        .apply { applyParams(this, *params) }
+        .use { stmt ->
+            stmt.executeQuery().use { rs ->
+                while (rs.next()) {
+                    action.invoke(rs)
+                }
+            }
+        }
 
 fun <T> Connection.select(sql: String, vararg params: Any?, action: ResultSet.() -> T): List<T> {
     val list = mutableListOf<T>()
-    val stmt = prepareStatement(sql).apply { applyParams(this, *params) }
-    val rs = stmt.executeQuery()
-    while (rs.next()) {
-        list.add(action.invoke(rs))
-    }
-    rs.close()
-    stmt.close()
+    prepareStatement(sql)
+            .apply { applyParams(this, *params) }
+            .use { stmt ->
+                stmt.executeQuery().use { rs ->
+                    while (rs.next()) {
+                        list.add(action.invoke(rs))
+                    }
+                }
+            }
     return list
 }
 
 fun <T> Connection.selectOne(sql: String, vararg params: Any?, action: ResultSet.() -> T): T? {
     var value: T? = null
-    val stmt = prepareStatement(sql).apply { applyParams(this, *params) }
-
-    params.forEachIndexed { i, any -> stmt.setObject(i + 1, any) }
-    val rs = stmt.executeQuery()
-    if (rs.next()) {
-        value = action.invoke(rs)
-    }
-    rs.close()
-    stmt.close()
+    prepareStatement(sql)
+            .apply { applyParams(this, *params) }
+            .use { stmt ->
+                params.forEachIndexed { i, any -> stmt.setObject(i + 1, any) }
+                stmt.executeQuery().use { rs ->
+                    if (rs.next()) {
+                        value = action.invoke(rs)
+                    }
+                }
+            }
     return value
 }
 
 fun Connection.delete(sql: String, vararg params: Any?): Int = update(sql, *params)
 fun Connection.insert(sql: String, vararg params: Any?): Int = update(sql, *params)
-fun Connection.update(sql: String, vararg params: Any?): Int {
-    val stmt = prepareStatement(sql).apply { applyParams(this, *params) }
-    val result = stmt.executeUpdate()
-    stmt.close()
-    return result
-}
+fun Connection.update(sql: String, vararg params: Any?): Int
+        = prepareStatement(sql)
+        .apply { applyParams(this, *params) }
+        .use { it.executeUpdate() }
 
-fun Connection.insertReturnKey(sql: String, vararg params: Any?): String? {
-    val stmt = prepareStatement(sql, Statement.RETURN_GENERATED_KEYS).apply { applyParams(this, *params) }
-    val result = stmt.executeUpdate()
-    var answer: String? = null
-    if (result == 1) {
-        val rs = stmt.generatedKeys
-        if (rs.next()) {
-            answer = rs.getString(1)
+fun Connection.insertReturnKey(sql: String, vararg params: Any?): String?
+        = prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        .apply { applyParams(this, *params) }
+        .use { stmt ->
+            val result = stmt.executeUpdate()
+            var answer: String? = null
+            if (result == 1) {
+                stmt.generatedKeys.use { rs ->
+                    if (rs.next()) {
+                        answer = rs.getString(1)
+                    }
+                }
+            }
+            return answer
         }
-        rs.close()
-    }
-    stmt.close()
-    return answer
-}
 
-fun <T> Connection.call(sql: String, action: CallableStatement.() -> T): T? {
-    val stmt = prepareCall(sql)
-    try {
-        return action.invoke(stmt)
-    } finally {
-        stmt.close()
-    }
-}
+fun <T> Connection.call(sql: String, action: CallableStatement.() -> T): T?
+        = prepareCall(sql).use { action.invoke(it) }
 
 private fun applyParams(stmt: PreparedStatement, vararg params: Any?) = stmt.apply {
     params.forEachIndexed { i, any ->
--- tekniq-jdbc/src/main/kotlin/io/tekniq/jdbc/TqDataSourceExt.kt
+++ tekniq-jdbc/src/main/kotlin/io/tekniq/jdbc/TqDataSourceExt.kt
@@ -1,13 +1,10 @@
 package io.tekniq.jdbc
 
-import java.sql.CallableStatement
-import java.sql.Connection
-import java.sql.ResultSet
+import java.sql.*
 import javax.sql.DataSource
 
 fun <T> DataSource.transaction(commitOnCompletion: Boolean = true, level: Int = Connection.TRANSACTION_READ_COMMITTED, boundary: Connection.() -> T): T? {
-    val conn = connection
-    try {
+    connection.use { conn ->
         conn.autoCommit = false
         conn.transactionIsolation = level
         val result = boundary.invoke(conn)
@@ -15,61 +12,35 @@ fun <T> DataSource.transaction(commitOnCompletion: Boolean = true, level: Int = 
             conn.commit()
         }
         return result
-    } finally {
-        conn.close()
     }
 }
 
 fun <T> DataSource.call(sql: String, action: CallableStatement.() -> T): T? {
-    val conn = connection
-    try {
+    connection.use { conn ->
         conn.autoCommit = false
         val x = conn.call(sql, action = action)
         conn.commit()
         return x
-    } finally {
-        conn.close()
     }
 }
 
-fun DataSource.select(sql: String, vararg params: Any?, action: ResultSet.() -> Unit) {
-    val conn = connection
-    try {
-        conn.select(sql, *params, action = action)
-    } finally {
-        conn.close()
-    }
-}
+fun DataSource.select(sql: String, vararg params: Any?, action: ResultSet.() -> Unit)
+        = connection.use { it.select(sql, *params, action = action) }
 
-fun <T> DataSource.select(sql: String, vararg params: Any?, action: ResultSet.() -> T): List<T> {
-    val conn = connection
-    try {
-        return conn.select(sql, *params, action = action)
-    } finally {
-        conn.close()
-    }
-}
+fun <T> DataSource.select(sql: String, vararg params: Any?, action: ResultSet.() -> T): List<T>
+        = connection.use { it.select(sql, *params, action = action) }
 
-fun <T> DataSource.selectOne(sql: String, vararg params: Any?, action: ResultSet.() -> T): T? {
-    val conn = connection
-    try {
-        return conn.selectOne(sql, *params, action = action)
-    } finally {
-        conn.close()
-    }
-}
+fun <T> DataSource.selectOne(sql: String, vararg params: Any?, action: ResultSet.() -> T): T?
+        = connection.use { it.selectOne(sql, *params, action = action) }
 
 fun DataSource.delete(sql: String, vararg params: Any?): Int = update(sql, *params)
 fun DataSource.insert(sql: String, vararg params: Any?): Int = update(sql, *params)
 fun DataSource.update(sql: String, vararg params: Any?): Int {
-    val conn = connection
-    try {
+    connection.use { conn ->
         conn.autoCommit = false
         val effected = conn.update(sql, *params)
         conn.commit()
         return effected
-    } finally {
-        conn.close()
     }
 }
 
