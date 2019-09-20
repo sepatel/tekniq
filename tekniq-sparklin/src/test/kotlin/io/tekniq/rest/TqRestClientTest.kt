42b17d3f2f5bd0f981b5cb5981c28587f1522cdb
Sejal Patel <sejal@tekniq.io>
2019-09-20 09:41:40 -0400

    Ignore invalid hostnames on self-signed certs
    Often times internal organizations are using self-signed certificates
    but have multiple DNS names mapped to the system. The request is that it
    is not any less risky to allow mismatched hostnames because they are
    only used for internal communication channels anyway.

--- tekniq-sparklin/src/test/kotlin/io/tekniq/rest/TqRestClientTest.kt
+++ tekniq-sparklin/src/test/kotlin/io/tekniq/rest/TqRestClientTest.kt
@@ -1,7 +1,7 @@
 package io.tekniq.rest
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.fail
+import org.junit.Assert.*
+import org.junit.Ignore
 import org.junit.Test
 
 class TqRestClientTest {
@@ -13,16 +13,35 @@ class TqRestClientTest {
         }
     }
 
+    @Ignore
     @Test
     fun failOnInsecureSite() {
         val client = TqRestClient()
-        client.get("https://216.58.210.164/finance") {
+        client.get("https://172.217.5.228") {
             assertEquals(-1, status)
         }
     }
 
+    @Ignore
     @Test
-    fun getFailsonInsecureSiteDueToPreviousCallDisablingSecurity() {
+    fun ignoreInsecureSite() {
+        val client = TqRestClient(allowSelfSigned = true)
+        client.get("https://test.tekniq.io/version") {
+            assertEquals(200, status)
+        }
+    }
+
+    @Test
+    fun ignoreHostnameValidation() {
+        val client = TqRestClient(ignoreHostnameVerifier = true)
+        client.get("https://172.217.5.228") {
+            assertNotEquals(-1, status)
+        }
+    }
+
+    @Ignore
+    @Test
+    fun getFailsOnInsecureSiteDueToPreviousCallDisablingSecurity() {
         failOnInsecureSite()
     }
 }
