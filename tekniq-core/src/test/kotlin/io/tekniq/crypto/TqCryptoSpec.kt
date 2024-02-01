6ba788a51756a047cbdfadfbae116e7156536905
Sejal Patel <sejal@tekniq.io>
2024-02-01 12:08:29 -0500

    Include hmac digest support

--- tekniq-core/src/test/kotlin/io/tekniq/crypto/TqCryptoSpec.kt
+++ tekniq-core/src/test/kotlin/io/tekniq/crypto/TqCryptoSpec.kt
@@ -1,6 +1,7 @@
 package io.tekniq.crypto
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 import io.tekniq.crypto.TqCryptography.decrypt
 import io.tekniq.crypto.TqCryptography.encrypt
 import kotlin.test.assertEquals
@@ -23,6 +24,15 @@ object TqCryptoSpec : DescribeSpec({
         it("correctly hashes via md5") {
             val hash = TqCryptography.md5("I am a flying purple monkey")
             assertEquals("e1a3401853a457a79917b7a59e975333", hash)
+        }
+    }
+
+    describe("Hmac SHA256 Digest") {
+        it("correctly matches the hash") {
+            val answer = "1c07f8a797b99a72a39b4542dc076cb810506914c1f7aeb00231cadf63129824"
+            val msg = "Simple Digest Message"
+            val digest = TqCryptography.hmac(msg, "Guardians of the Galaxy")
+            digest shouldBe answer
         }
     }
 })
