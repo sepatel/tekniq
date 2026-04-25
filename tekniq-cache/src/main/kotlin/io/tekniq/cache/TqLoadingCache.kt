d64e9679fb649e6d0798a1af99a7b2293ed9de18
Sejal Patel <sejal@playerzero.ai>
2026-04-25 15:53:13 -0400

    Upgrade dependencies

--- tekniq-cache/src/main/kotlin/io/tekniq/cache/TqLoadingCache.kt
+++ tekniq-cache/src/main/kotlin/io/tekniq/cache/TqLoadingCache.kt
@@ -1,11 +1,11 @@
 package io.tekniq.cache
 
+import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
-import com.github.benmanes.caffeine.cache.LoadingCache
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import kotlin.collections.MutableMap.MutableEntry
 
-open class TqLoadingCache<K, V>(
+open class TqLoadingCache<K : Any, V : Any>(
     val expireAfterAccess: Long? = null,
     val expireAfterWrite: Long? = null,
     val refreshAfterWrite: Long? = null,
@@ -13,25 +13,24 @@ open class TqLoadingCache<K, V>(
     val recordStats: Boolean = false,
     private val loader: (key: K) -> V?
 ) : TqCacheMap<K, V> {
-    private val cacheLoader: LoadingCache<K, V> = Caffeine
+    private val cache: Cache<K, V> = Caffeine
         .newBuilder()
         .also { builder ->
             expireAfterAccess?.also { builder.expireAfterAccess(expireAfterAccess, MILLISECONDS) }
             expireAfterWrite?.also { builder.expireAfterWrite(expireAfterWrite, MILLISECONDS) }
             maximumSize?.also { builder.maximumSize(maximumSize) }
-            refreshAfterWrite?.also { builder.refreshAfterWrite(refreshAfterWrite, MILLISECONDS) }
             if (recordStats) builder.recordStats()
         }
-        .build { key -> loader(key) }
-    private val map = cacheLoader.asMap()
+        .build()
+    private val map = cache.asMap()
     override val entries: MutableSet<MutableEntry<K, V>>
         get() = map.entries
     override val keys: MutableSet<K>
         get() = map.keys
     override val size: Int
-        get() = cacheLoader.estimatedSize().toInt()
+        get() = cache.estimatedSize().toInt()
     override val stats: TqCacheStats
-        get() = cacheLoader.stats().let {
+        get() = cache.stats().let {
             TqCacheStats(
                 hitCount = it.hitCount(),
                 missCount = it.missCount(),
@@ -52,7 +51,12 @@ open class TqLoadingCache<K, V>(
     override fun put(key: K, value: V): V? = map.put(key, value)
     override fun putAll(from: Map<out K, V>) = map.putAll(from)
     override fun remove(key: K): V? = map.remove(key)
-    override fun get(key: K): V? = cacheLoader.get(key)
-    override fun cleanUp() = cacheLoader.cleanUp()
+    override fun get(key: K): V? {
+        cache.getIfPresent(key)?.let { return it }
+        val loaded = loader(key) ?: return null
+        cache.put(key, loaded)
+        return loaded
+    }
+    override fun cleanUp() = cache.cleanUp()
 }
 
